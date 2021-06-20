5b56679be3ecc9dcf2d122a51629d84c5f29f40e
Sebasrestrepom <sebasrestrepom@gmail.com>
2021-06-20 00:28:05 -0500

    migratios

--- src/http/app.module.ts
+++ src/http/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as ormconfig from './ormconfig';
+import * as ormconfig from '../ormconfig';
 import { PatientController } from './http/controller/patient/save-a-new-patient/SaveANewPatientController';
 import { SQLPatientRepository } from './core/infrastructure/patient/SQLPatientRepository';
 import { PatientService } from './core/use_cases/patient/SaveANewPatient';
